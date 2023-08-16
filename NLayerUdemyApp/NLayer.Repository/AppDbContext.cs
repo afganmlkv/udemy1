7d3d54c74a4080e43512c4a73cb12eb653087e81
afganmlkv <afganmlkv@gmail.com>
2023-08-16 18:26:52 +0400

    SeedData added

--- NLayerUdemyApp/NLayer.Repository/AppDbContext.cs
+++ NLayerUdemyApp/NLayer.Repository/AppDbContext.cs
@@ -22,6 +22,23 @@ namespace NLayer.Repository
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+            {
+                Id = 1,
+                Color = "Kirmizi",
+                Height = 100,
+                Width = 100,
+                ProductId = 1,
+            },
+            new ProductFeature()
+            {
+                Id = 2,
+                Color = "Mavi",
+                Height = 100,
+                Width = 100,
+                ProductId = 2,
+            });
            
             base.OnModelCreating(modelBuilder);
         }
