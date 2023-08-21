dfece1923360b49655a2ab2074afac789f3b202a
afganmlkv <afganmlkv@gmail.com>
2023-08-21 17:32:31 +0400

    FluentValidation Filter

--- NLayerUdemyApp/NLayer.API/Program.cs
+++ NLayerUdemyApp/NLayer.API/Program.cs
@@ -1,6 +1,8 @@
 
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NLayer.API.Filters;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -23,8 +25,12 @@ namespace NLayer.API
 
             // Add services to the container.
 
-            builder.Services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
+            builder.Services.AddControllers(options => options.Filters.Add(new ValidateFilterAttribute())).AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
